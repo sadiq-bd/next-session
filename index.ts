f20afb98cf996bd259a040df79e22df1dee34ca1
Sadiq <sadiq0698@gmail.com>
2025-05-30 15:32:13 +0600

    update

--- index.ts
+++ index.ts
@@ -3,19 +3,18 @@ import { SignJWT, jwtVerify } from 'jose'
 import { cookies } from 'next/headers'
  
 const secretKey = process.env.SESSION_SECRET;
-const encodedKey = new TextEncoder().encode(secretKey);
  
 export async function encrypt(payload: {}) {
   return new SignJWT(payload)
     .setProtectedHeader({ alg: 'HS256' })
     .setIssuedAt()
     .setExpirationTime('7d')
-    .sign(encodedKey);
+    .sign(new TextEncoder().encode(secretKey));
 }
  
 export async function decrypt(session: string | undefined = '') {
   try {
-    const { payload } = await jwtVerify(session, encodedKey, {
+    const { payload } = await jwtVerify(session, new TextEncoder().encode(secretKey), {
       algorithms: ['HS256'],
     })
     return payload;
