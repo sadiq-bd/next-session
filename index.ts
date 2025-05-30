1f866f8de383e61d5d231711a564b5b76e6cfc8b
Sadiq <sadiq0698@gmail.com>
2025-05-30 14:31:35 +0600

    initial

---
+++ index.ts
@@ -0,0 +1,60 @@
+import 'server-only'
+import { SignJWT, jwtVerify } from 'jose'
+import { cookies } from 'next/headers'
+ 
+const secretKey = '2bc4b0799a69d7b799a69a3c87b2782dbc4b7b0f4eb5278823f4e2a3c838b7b5';
+const encodedKey = new TextEncoder().encode(secretKey);
+ 
+export async function encrypt(payload: {}) {
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime('7d')
+    .sign(encodedKey);
+}
+ 
+export async function decrypt(session: string | undefined = '') {
+  try {
+    const { payload } = await jwtVerify(session, encodedKey, {
+      algorithms: ['HS256'],
+    })
+    return payload;
+  } catch (error) {
+    // console.log('Failed to verify session');
+    return null;
+  }
+}
+
+export async function getSession(key?: string, fallback: string|null = null) {
+  const session = (await cookies()).get('app_session')?.value;
+  const payload = await decrypt(session);
+  // @ts-ignore
+  if (key) return payload[key] ?? fallback;
+  return payload;
+}
+
+export async function setSession(key: string, value: string) {
+  const session = (await cookies()).get('app_session')?.value;
+  let payload = (await decrypt(session)) ?? {};
+ 
+  payload[key] = value;
+
+  const updatedSession = await encrypt(payload);
+   
+  const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+ 
+  const cookieStore = await cookies();
+  cookieStore.set('app_session', updatedSession, {
+    httpOnly: true,
+    secure: true,
+    expires: expires,
+    sameSite: 'lax',
+    path: '/',
+  });
+
+}
+
+export async function destroySession() {
+  const cookieStore = await cookies()
+  cookieStore.delete('session')
+}
